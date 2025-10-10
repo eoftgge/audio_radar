e35349a668f4d3534309b6f0c26cfb32c6ab8fe0
setkg <sandaar.ykt@gmail.com>
2025-10-10 23:25:23 +0900

    change logger to file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-#![windows_subsystem = "windows"]
-
 use audio_radar::audio::start_capture_audio;
 use audio_radar::handler::handler;
 use audio_radar::types::RadarMessage;
+use simple_file_logger::{init_logger, LogLevel};
 
 fn main() {
-    
+    init_logger("audio_radar", LogLevel::Info).unwrap();
     let (tx_radar, rx_radar) = std::sync::mpsc::channel::<RadarMessage>();
     std::thread::spawn(move || start_capture_audio(tx_radar));
 
