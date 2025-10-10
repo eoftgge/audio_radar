595b388dbdc3e2be241a234056a716083b239742
setkg <sandaar.ykt@gmail.com>
2025-10-10 23:27:13 +0900

    add error message box windows

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,24 @@ use audio_radar::audio::start_capture_audio;
 use audio_radar::handler::handler;
 use audio_radar::types::RadarMessage;
 use simple_file_logger::{init_logger, LogLevel};
+use windows::core::PCWSTR;
+use windows::Win32::UI::WindowsAndMessaging::{MessageBoxW, MB_ICONERROR, MB_OK};
+
+fn show_error(msg: &str) {
+    use std::ffi::OsStr;
+    use std::os::windows::ffi::OsStrExt;
+
+    let wide: Vec<u16> = OsStr::new(msg).encode_wide().chain(Some(0)).collect();
+
+    unsafe {
+        MessageBoxW(
+            None,
+            PCWSTR(wide.as_ptr()),
+            PCWSTR(wide.as_ptr()),
+            MB_OK | MB_ICONERROR,
+        );
+    }
+}
 
 fn main() {
     init_logger("audio_radar", LogLevel::Info).unwrap();
@@ -13,5 +31,6 @@ fn main() {
     if let Err(err) = handler(rx_radar) {
         log::error!("{}", err);
         log::warn!("aborting...");
+        show_error(&format!("{}", err));
     }
 }
