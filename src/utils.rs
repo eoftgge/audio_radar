2a6d2d6c810128738e5c11e4269c4c56b0b4f248
setkg <sandaar.ykt@gmail.com>
2025-10-10 19:36:34 +0900

    delete comment

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,5 @@
 use windows::Win32::Foundation::COLORREF;
 
 pub fn colorref_from_rgb(r: u8, g: u8, b: u8) -> COLORREF {
-    // Windows RGB macro: (r) | (g << 8) | (b << 16)
     COLORREF((r as u32) | ((g as u32) << 8) | ((b as u32) << 16))
 }
